267bd5f18a399e129475049ad7f1fee911e6608e
Mandrew1 <ajcarras@me.com>
2017-11-23 13:10:16 -0500

    Minor Camera Tweaks
    Worked On Fixing Camera Rotations. Not Done Yet.
    Version: 0.0.1

--- Ski Test/Assets/My Assets/Scripts/CameraFollow.cs
+++ Ski Test/Assets/My Assets/Scripts/CameraFollow.cs
@@ -6,12 +6,9 @@ public class CameraFollow : MonoBehaviour {
     public GameObject player;
     	
 	// Update is called once per frame
-	void FixedUpdate () {
-        if (player)
-        {
-            gameObject.transform.position = new Vector3(player.transform.position.x, player.transform.position.y + 2, player.transform.position.z - 5);
-            transform.LookAt(player.transform);
+	void LateUpdate () {
         
-        }
+            gameObject.transform.position = new Vector3(player.transform.position.x, player.transform.position.y + 2, player.transform.position.z - 5);
+            transform.LookAt(player.transform, transform.up);
     }
   }
